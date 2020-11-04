e93257f0c7d87ffe34f9a159baccb1d4c52b9318
Hamster56 <72356708+Hamster56@users.noreply.github.com>
2020-11-04 18:42:10 +0000

    Add files via upload

---
+++ HeckBot.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+
+bot = discord.Client()
+
+bot = commands.Bot(command_prefix='!')
+
+heck_list= []
+
+#heck_emoji = bot.get_emoji(631567436373688320)
+
+@bot.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(bot))
+
+@bot.command(pass_context= True)
+async def heck(ctx, members: commands.Greedy[discord.Member]) :
+    for x in members :
+        heck_list.append(x.id)
+        await ctx.send(f"{x.name} added to list")
+    print(heck_list)
+
+@bot.event
+async def on_message(message) :
+    if message.author.id in heck_list :
+        print('Found in list')
+        await message.add_reaction('<:Heck:773607864706269244>')
+    await bot.process_commands(message)
+
+bot.run('NzczNTU3OTcxODU4NzUxNDk4.X6K-AA.8znFQmA9Aa2pRwJhl7qIxR0UN3A')
